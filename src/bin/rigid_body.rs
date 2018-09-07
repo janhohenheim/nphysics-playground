0ec73772064a9e710b338fdf084bd3394f97a37b
Jeremy Stucki <stucki.jeremy@gmail.com>
2018-09-07 21:51:20 +0200

    Print collider position
    Sorry for the late commit @jnferner

--- src/bin/rigid_body.rs
+++ src/bin/rigid_body.rs
@@ -2,25 +2,27 @@ use ncollide2d::shape::{Cuboid, ShapeHandle};
 
 use nalgebra as na;
 use nphysics2d::math::{Isometry, Vector};
-use nphysics2d::object::Material;
+use nphysics2d::object::{Material, Collider};
 use nphysics2d::volumetric::Volumetric;
 use nphysics2d::world::World;
 
+use std::{thread, time};
+
 fn main() {
     let mut world = World::new();
-    world.set_gravity(Vector::y() * -9.81);
+    world.set_gravity(Vector::new(0.0, 0.0));
 
     let cuboid = ShapeHandle::new(Cuboid::new(Vector::new(1.0, 2.0)));
     let local_inertia = cuboid.inertia(0.1);
     let local_center_of_mass = cuboid.center_of_mass();
     let rigid_body_handle = world.add_rigid_body(
-        Isometry::new(Vector::x() * 2.0, na::zero()),
+        Isometry::new(Vector::new(2.0, 10.0), na::zero()),
         local_inertia,
         local_center_of_mass,
     );
 
     // Create a collider attached to a previously-added rigid-body with handle `rigid_body_handle`.
-    let material = Material::new(0.5, 0.3); // Custom material.
+    let material = Material::default(); // Custom material.
     let _collider_handle = world.add_collider(
         0.04,
         cuboid,
@@ -31,5 +33,9 @@ fn main() {
 
     loop {
         world.step();
+        let collider = world.collider(_collider_handle).expect("The unexpected");
+        println!("{:#?}", collider.position());
+
+        thread::sleep(time::Duration::from_millis(1000));
     }
 }
