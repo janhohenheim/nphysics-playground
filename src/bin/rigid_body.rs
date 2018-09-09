8706de9b4aa0d20157432937106ba34a665206f5
Jan Nils Ferner <jn_ferner@hotmail.de>
2018-09-09 21:57:30 +0200

    Try to use a polyline

--- src/bin/rigid_body.rs
+++ src/bin/rigid_body.rs
@@ -1,7 +1,7 @@
-use ncollide2d::shape::{Cuboid, ShapeHandle};
+use ncollide2d::shape::{Polyline, ShapeHandle};
 
-use nalgebra as na;
-use nphysics2d::math::{Isometry, Vector};
+//use nalgebra as na;
+use nphysics2d::math::{Isometry, Point, Vector};
 use nphysics2d::object::Material;
 use nphysics2d::volumetric::Volumetric;
 use nphysics2d::world::World;
@@ -10,22 +10,27 @@ use std::{thread, time};
 
 fn main() {
     let mut world = World::new();
-    world.set_gravity(Vector::new(0.0, 0.0));
+    world.set_gravity(Vector::y() * 9.81);
 
-    let cuboid = ShapeHandle::new(Cuboid::new(Vector::new(1.0, 2.0)));
-    let local_inertia = cuboid.inertia(0.1);
-    let local_center_of_mass = cuboid.center_of_mass();
+    let polyline = ShapeHandle::new(Polyline::new(vec![
+        Point::new(1.0, 2.0),
+        Point::new(2.0, 2.0),
+        Point::new(2.0, 3.0),
+        Point::new(1.0, 3.0),
+    ]));
+    let local_inertia = polyline.inertia(0.1);
+    let local_center_of_mass = polyline.center_of_mass();
     let rigid_body_handle = world.add_rigid_body(
-        Isometry::new(Vector::new(2.0, 10.0), na::zero()),
+        Isometry::new(Vector::new(2.0, 10.0), 3.0),
         local_inertia,
         local_center_of_mass,
     );
 
     // Create a collider attached to a previously-added rigid-body with handle `rigid_body_handle`.
     let material = Material::default(); // Custom material.
-    let _collider_handle = world.add_collider(
+    let collider_handle = world.add_collider(
         0.04,
-        cuboid,
+        polyline,
         rigid_body_handle,
         Isometry::identity(),
         material,
@@ -33,9 +38,17 @@ fn main() {
 
     loop {
         world.step();
-        let collider = world.collider(_collider_handle).expect("The unexpected");
-        println!("{:#?}", collider.position());
-
-        thread::sleep(time::Duration::from_millis(1000));
+        let collider = world
+            .collider(collider_handle)
+            .expect("Collider handle has invalid");
+        println!("{}", collider.position());
+        let shape: &Polyline<_> = collider
+            .shape()
+            .as_shape()
+            .expect("Failed to cast shapehandle to a cuboid");
+        for vertex in shape.vertices() {
+            println!("{}", vertex);
+        }
+        thread::sleep(time::Duration::from_millis(1000 / 60));
     }
 }
