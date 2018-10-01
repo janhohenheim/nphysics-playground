3b65a0bbad1817fd3331a79e47e521f10ff9d0a1
Jan Nils Ferner <jn_ferner@hotmail.de>
2018-10-01 15:50:11 +0200

    Apply forces

--- src/bin/rigid_body.rs
+++ src/bin/rigid_body.rs
@@ -2,16 +2,51 @@ use ncollide2d::shape::{ConvexPolygon, ShapeHandle};
 
 //use nalgebra as na;
 use nalgebra::base::{Scalar, Vector2};
-use nphysics2d::math::{Isometry, Point, Vector};
-use nphysics2d::object::Material;
+use nphysics2d::force_generator::ForceGenerator;
+use nphysics2d::math::{Force, Isometry, Point, Vector, Velocity};
+use nphysics2d::object::{BodyHandle, BodySet, Material};
+use nphysics2d::solver::IntegrationParameters;
 use nphysics2d::volumetric::Volumetric;
 use nphysics2d::world::World;
-
 use std::{thread, time};
+
+#[derive(Default)]
+pub struct RadialForce {
+    parts: Vec<BodyHandle>, // Body parts affected by the force generator.
+    is_done: bool,
+}
+
+impl RadialForce {
+    /// Add a body part to be affected by this force generator.
+    pub fn add_body_part(&mut self, body: BodyHandle) {
+        self.parts.push(body)
+    }
+}
+
+impl ForceGenerator<f64> for RadialForce {
+    fn apply(&mut self, _: &IntegrationParameters<f64>, bodies: &mut BodySet<f64>) -> bool {
+        if self.is_done {
+            return false;
+        }
+        self.is_done = true;
+        for &handle in &self.parts {
+            // Generate the force only if the body has not been removed from the world.
+            if bodies.contains(handle) {
+                let mut body = bodies.body_part_mut(handle);
+                let force = Force::from_slice(&[5.00, 2.00, 0.5]);
+                body.apply_force(&force);
+            }
+        }
+
+        // If `false` is returned, the physis world will remove
+        // this force generator after this call.
+        true
+    }
+}
 
 fn main() {
     let mut world = World::new();
-    world.set_gravity(Vector::y() * 9.81);
+    //world.set_gravity(Vector::y() * 9.81);
 
     let polygon = ShapeHandle::new(
         ConvexPolygon::try_new(vec![
@@ -19,7 +54,8 @@ fn main() {
             Point::new(2.0, 2.0),
             Point::new(2.0, 3.0),
             Point::new(1.0, 3.0),
-        ]).unwrap(),
+        ])
+        .unwrap(),
     );
     let local_inertia = polygon.inertia(0.1);
     let local_center_of_mass = polygon.center_of_mass();
@@ -40,7 +76,7 @@ fn main() {
     );
 
     let rigid_body_handle_two = world.add_rigid_body(
-        Isometry::new(Vector::new(1.0, 10.0), 3.0),
+        Isometry::new(Vector::new(20.0, 25.0), 3.0),
         local_inertia,
         local_center_of_mass,
     );
@@ -55,11 +91,12 @@ fn main() {
 
     let sensor_shape = ShapeHandle::new(
         ConvexPolygon::try_new(vec![
-            Point::new(-5.0, -5.0),
-            Point::new(5.0, -5.0),
-            Point::new(5.0, 5.0),
-            Point::new(-5.0, 5.0),
-        ]).unwrap(),
+            Point::new(-50.0, -50.0),
+            Point::new(50.0, -50.0),
+            Point::new(50.0, 50.0),
+            Point::new(-50.0, 50.0),
+        ])
+        .unwrap(),
     );
 
     let sensor_position = Isometry::new(Vector::new(0.0, 0.0), 0.0);
@@ -69,12 +106,16 @@ fn main() {
         sensor_position,   // The relative position of this sensor wrt. its parent.
     );
 
+    let mut force_generator = RadialForce::default();
+    force_generator.add_body_part(rigid_body_handle);
+    world.add_force_generator(force_generator);
+
     loop {
         world.step();
         let collider = world
             .collider(collider_handle)
             .expect("Collider handle was invalid");
-        //println!("{}", collider.position());
+        println!("{}", collider.position());
         let shape: &ConvexPolygon<_> = collider
             .shape()
             .as_shape()
@@ -95,8 +136,8 @@ fn main() {
             //println!("[{}, {}]", rotated_x, rotated_y);
         }
 
-        for contact in world.contact_events() {
-            println!("{:?}", contact);
+        for proximity in world.proximity_events() {
+            //println!("{:?}", proximity);
         }
 
         thread::sleep(time::Duration::from_millis(1000 / 60));
